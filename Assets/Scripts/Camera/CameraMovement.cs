a3e3b8d932eaa633ac6705a8034ffedbc7a6447c
Mark Obeldobel <markobeldobel@yahoo.com>
2019-01-24 13:20:29 -0500

    Camera Movement is now working.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    [SerializeField] private float cameraMoveSpeed;
+
+    private float horizCap;
+    private float vertCap;
+    private float currHorizVal;
+    private float currVertVal;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+    {
+        horizCap = 12.0f;
+        vertCap = 5.0f;
+        currHorizVal = 0.0f;
+        currVertVal = 0.0f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow))
-        {
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
+	void Update()
+    {
 
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
+        //check to see if you are moving the camera up, down, left, or right
+        MoveCamera(Input.GetAxis("SecondaryCommandHoriz"), Input.GetAxis("SecondaryCommandVert"));
+    }
 
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
+    //takes in input values and moves the camera based on the values
+    public void MoveCamera(float right, float up)
+    {
+        if (right > 0 && currHorizVal < horizCap)
         {
+            transform.position += transform.right * cameraMoveSpeed * Time.deltaTime;
+            currHorizVal += transform.right.magnitude * cameraMoveSpeed * Time.deltaTime;
+        }
+        else if (right < 0 && currHorizVal > - horizCap)
+        {
+            transform.position += -transform.right * cameraMoveSpeed * Time.deltaTime;
+            currHorizVal -= transform.right.magnitude * cameraMoveSpeed * Time.deltaTime;
+        }
 
+        if (up > 0 && currVertVal < vertCap)
+        {
+            transform.position += transform.up * cameraMoveSpeed * Time.deltaTime;
+            currVertVal += transform.right.magnitude * cameraMoveSpeed * Time.deltaTime;
+        }
+        else if (up < 0 && currVertVal > -vertCap)
+        {
+            transform.position += -transform.up * cameraMoveSpeed * Time.deltaTime;
+            currVertVal -= transform.right.magnitude * cameraMoveSpeed * Time.deltaTime;
         }
     }
 }

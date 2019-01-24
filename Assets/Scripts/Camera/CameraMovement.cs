2301c1f993cfc97600424d7e04122d2cbf59a521
Mark Obeldobel <markobeldobel@yahoo.com>
2019-01-24 13:47:32 -0500

    Added camera zoom.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -6,10 +6,14 @@ public class CameraMovement : MonoBehaviour {
 
     [SerializeField] private float cameraMoveSpeed;
 
-    private float horizCap;
-    private float vertCap;
+    [SerializeField] private float horizCap;
+    [SerializeField] private float vertCap;
     private float currHorizVal;
     private float currVertVal;
+
+    [SerializeField] private float zoomOutMax;
+    [SerializeField] private float zoomInMin;
+    [SerializeField] private float scrollSpeed;
 
 	// Use this for initialization
 	void Start()
@@ -26,6 +30,7 @@ public class CameraMovement : MonoBehaviour {
 
         //check to see if you are moving the camera up, down, left, or right
         MoveCamera(Input.GetAxis("SecondaryCommandHoriz"), Input.GetAxis("SecondaryCommandVert"));
+        ZoomCamera(Input.GetAxis("Mouse Scrollwheel"), zoomInMin, zoomOutMax);
     }
 
     //takes in input values and moves the camera based on the values
@@ -51,6 +56,22 @@ public class CameraMovement : MonoBehaviour {
         {
             transform.position += -transform.up * cameraMoveSpeed * Time.deltaTime;
             currVertVal -= transform.right.magnitude * cameraMoveSpeed * Time.deltaTime;
+        }
+    }
+
+    public void ZoomCamera(float mouseScroll, float min, float max)
+    {
+        if (gameObject.GetComponent<Camera>() != null)
+        {
+            Camera obtainedCam = gameObject.GetComponent<Camera>();
+            if (mouseScroll > 0 && obtainedCam.orthographicSize > min)
+            {
+                obtainedCam.orthographicSize -= scrollSpeed;
+            }
+            else if (mouseScroll < 0 && obtainedCam.orthographicSize < max)
+            {
+                obtainedCam.orthographicSize += scrollSpeed;
+            }
         }
     }
 }

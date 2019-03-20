a8e1920ce43c8e237ba1df13bc95fddf93c1b3b0
Naveen Sampath <ns7767@ad.rit.edu>
2019-03-19 20:49:58 -0400

    Volume control logic is done.

--- Assets/Scripts/UI/VolumeHandler.cs
+++ Assets/Scripts/UI/VolumeHandler.cs
@@ -4,23 +4,85 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class VolumeHandler : MonoBehaviour
-{
-    [SerializeField] private Image[] fillVolume;
+{ 
+    [SerializeField] private Button decreaseButton;
+    [SerializeField] private Button increaseButton;
+    [SerializeField] private GameObject[] fillVolume;
+    [SerializeField] private Sprite[] sprites;
+
+    [SerializeField] private int max;
+    [SerializeField] private int min;
+    public int currentIndex;
+
+    private float volumeControl;
+    private float maxVolume;
+    private float minVolume;
+    private int count;
+
+    private int indexCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        max = 5;
+        min = 0;
+        currentIndex = 0;
+        maxVolume = 1;
+        minVolume = 0;
+        count = 1;
+        indexCount = 5;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       
     }
-
-
-    private void OnMouseDown()
+    public void ChangeVolume(bool isPressed)
     {
-        
+        currentIndex = Mathf.Clamp(currentIndex + (isPressed ? count : -count), min, max);     
+       
+        if(isPressed)
+        {
+            //Debug.Log("Increase the volume");
+            volumeControl += 0.2f;
+            
+            if (volumeControl >= maxVolume)
+            {
+                volumeControl = maxVolume;
+            }
+            if (indexCount >= max)
+            {
+                indexCount = max;
+                fillVolume[indexCount].GetComponent<Image>().sprite = sprites[0];
+            }
+            else
+            {
+                indexCount++;
+                fillVolume[indexCount - 1].GetComponent<Image>().sprite = sprites[0];
+
+            }
+            
+        }
+        if(!isPressed)
+        {
+            //Debug.Log("Decrease the volume");
+            volumeControl -= 0.2f;
+          
+            if (volumeControl <= minVolume)
+            {
+                volumeControl = minVolume;
+            }
+            if (indexCount <= min)
+            {
+                indexCount = min;
+                fillVolume[indexCount].GetComponent<Image>().sprite = sprites[1];
+            }
+            else
+            {
+                indexCount--;
+
+                fillVolume[indexCount + 1].GetComponent<Image>().sprite = sprites[1];
+            }
+            
+        }
+        Debug.Log("indexCount:    " + indexCount);
     }
 }
